8d855ee84d011c575aeb16c041e54d5f36d6e058
Brain-Studios <143759728+MrGrumpycat19@users.noreply.github.com>
2025-05-29 15:19:43 -0700

    index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const youtube = google.youtube({
 });
 
 // Constants
-const CHANNEL_NAME_TEMPLATE = "🟥 Subs: ";
+const CHANNEL_NAME_TEMPLATE = "🟥\u00A0Subs: ";
 const POLL_INTERVAL = 10 * 60 * 1000; // 10 minutes
 
 // Format subscriber count
